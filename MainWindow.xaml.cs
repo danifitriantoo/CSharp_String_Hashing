a9476eae5e0e6d8865785c446ecf344f59423c85
Dani Fitrianto Nugraha <60064236+danifitriantoo@users.noreply.github.com>
2021-05-04 23:12:37 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace String_Hashing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string hash = "foxle@rn";
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            // Get String
+            byte[] data = UTF8Encoding.UTF8.GetBytes(txtString.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+
+                    // Start Encryption
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+
+                    // Passing Result To Textbox
+                    txtEncrypt.Text = Convert.ToBase64String(result, 0, result.Length);
+                }
+            }
+        }
+
+        private void btnEncrypt_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            // Get String
+            byte[] data = Convert.FromBase64String(txtEncrypt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+
+                    // Start Decrypting
+                    ICryptoTransform transform = tripDes.CreateDecryptor();
+                    byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+
+                    // Passing Result To Textbox
+                    txtDecrypt.Text = UTF8Encoding.UTF8.GetString(result);
+                }
+            }
+        }
+    }
+}
